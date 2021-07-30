6679b608805872d3c17234bf350719a73a4f40e3
Kyle Kotowick <kyle@kotowick.ca>
2021-07-30 17:04:04 -0400

    Fix incorrect replacements in output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "stdout" {
   // If it was in Linux, we must replace the magic string with quotes
-  value = local.is_windows ? local.stdout : replace(replace(local.stdout, "__TF_MAGIC_QUOTE_STRING", "\""), "__TF_MAGIC_NEWLINE_STRING", "\n")
+  value = local.stdout
 }
 
 output "stderr" {
   // If it was in Linux, we must replace the magic string with quotes
-  value = local.is_windows ? local.stderr : replace(replace(local.stderr, "__TF_MAGIC_QUOTE_STRING", "\""), "__TF_MAGIC_NEWLINE_STRING", "\n")
+  value = local.stderr
 }
 
 output "exitstatus" {
