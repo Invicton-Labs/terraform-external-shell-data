23f9839eeeffe128c6eafd94738e5620242084cb
KyleKotowick <kotowick@invictonlabs.com>
2022-06-26 19:27:18 -0400

    Add testing for sensitive outputs

--- tester/main.tf
+++ tester/main.tf
@@ -12,7 +12,7 @@ locals {
     name => keys(config)
   }
 
-  expected_output_fields = ["expected_stdout", "expected_stderr", "expected_exit_code"]
+  expected_output_fields = ["expected_stdout", "expected_stderr", "expected_exit_code", "expected_stdout_sensitive", "expected_stderr_sensitive"]
 
   tests_invalid_fields = {
     for name, keys in local.tests_fields :
@@ -29,7 +29,7 @@ locals {
         "fail_on_nonzero_exit_code",
         "fail_on_stderr",
         "fail_on_timeout",
-        "platforms"
+        "platforms",
       ], local.expected_output_fields), field)
     ]
   }
@@ -95,6 +95,24 @@ locals {
       contains(local.tests_fields[name], "expected_stdout") ? config.expected_stdout != module.tests[name].stdout ? ["Incorrect value for stdout: expected \"${config.expected_stdout}\", got \"${module.tests[name].stdout}\""] : [] : [],
       contains(local.tests_fields[name], "expected_stderr") ? config.expected_stderr != module.tests[name].stderr ? ["Incorrect value for stderr: expected \"${config.expected_stderr}\", got \"${module.tests[name].stderr}\""] : [] : [],
       contains(local.tests_fields[name], "expected_exit_code") ? config.expected_exit_code != module.tests[name].exit_code ? ["Incorrect value for exit code: expected \"${config.expected_exit_code == null ? "null" : config.expected_exit_code}\", got \"${module.tests[name].exit_code == null ? "null" : module.tests[name].exit_code}\""] : [] : [],
+      contains(local.tests_fields[name], "expected_stdout_sensitive") ? (
+        config.expected_stdout_sensitive ? (
+          // If we expect it to be sensitive, make sure we can nonsensitive it
+          !can(nonsensitive(module.tests[name].stdout)) ? ["Incorrect sensitivity for stdout: expected a sensitive value, but received a non-sensitive value."] : []
+          ) : (
+          // If we expect it to be nonsensitive, make sure we can NOT nonsensitive it
+          can(nonsensitive(module.tests[name].stdout)) ? ["Incorrect sensitivity for stdout: expected a non-sensitive value, but received a sensitive value."] : []
+        )
+      ) : [],
+      contains(local.tests_fields[name], "expected_stderr_sensitive") ? (
+        config.expected_stderr_sensitive ? (
+          // If we expect it to be sensitive, make sure we can nonsensitive it
+          !can(nonsensitive(module.tests[name].stderr)) ? ["Incorrect sensitivity for stderr: expected a sensitive value, but received a non-sensitive value."] : []
+          ) : (
+          // If we expect it to be nonsensitive, make sure we can NOT nonsensitive it
+          can(nonsensitive(module.tests[name].stderr)) ? ["Incorrect sensitivity for stderr: expected a non-sensitive value, but received a sensitive value."] : []
+        )
+      ) : [],
     ])
   }
 
@@ -111,5 +129,9 @@ module "assert_expected_output" {
   source        = "Invicton-Labs/assertion/null"
   version       = "~>0.2.1"
   condition     = local.incorrect_output_err_msg == null
-  error_message = local.incorrect_output_err_msg
+  error_message = try(nonsensitive(local.incorrect_output_err_msg), local.incorrect_output_err_msg)
+}
+
+output "checked" {
+  value = module.assert_expected_output.checked
 }
