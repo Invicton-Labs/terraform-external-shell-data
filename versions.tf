c62927cfb9ffb51cbde6727caed80e805abd566a
KyleKotowick <kotowick@invictonlabs.com>
2022-06-26 23:11:30 -0400

    Update TF versions

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.13, !=0.15.0, !=0.15.1, !=0.15.2, !=0.15.3"
   required_providers {
     external = {
       source  = "hashicorp/external"
