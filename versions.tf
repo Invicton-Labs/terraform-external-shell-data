ba7324b25a3dc7da754974700bc5c99a21932467
KyleKotowick <kotowick@invictonlabs.com>
2023-03-26 13:39:48 -0400

    Fix versions and output

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   // 0.15.0 - 0.15.3 had a bug where it threw an error if an output
   // was marked as sensitive.
-  required_version = ">= 0.13, !=0.15.0, !=0.15.1, !=0.15.2, !=0.15.3"
+  required_version = ">= 0.13.1, !=0.15.0, !=0.15.1, !=0.15.2, !=0.15.3, !=1.3.0"
   required_providers {
     external = {
       source  = "hashicorp/external"
